9c8d8e9672f7c08c8e5d5443253a7745b37d476f
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2018-04-26 17:32:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ if(message.content === "magie!eau"){ //magie
 }) 
 }
 if(message.content === "magie!feu"){ //magie
-    message.channel.send("**${member.user.username}** lance une ataque magique de feu", {
+    message.channel.send("**${member}** lance une ataque magique de feu", {
     file: "http://www.cheminee-poele-bordeaux.fr/wp-content/uploads/2014/09/flamme-gif.gif"
 }) 
 }
