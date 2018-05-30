fc8a211614056cd7d5216ce9ad0355860b7d85ed
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2018-05-30 18:05:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const client = new Discord.Client();
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [],  xp: []}).write()
 
 var prefix = ("sigbot!")
 
 bot.on('ready', function() {
     bot.user.setUsername("Sigbot")
-    bot.user.setPresence({ game: { name: 'faire des crepe pour niko'}, status: 'online'})
+    bot.user.setPresence({ game: { name: 'faire greve'}, status: 'invisible'})
     console.log("Connected")});
 
 bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
+    var msgauthor = message.author.id;
+
+    if(message.author.bot)return;
+
+if(!db.get("xp").find({user : msgauthor}).value()){
+    db.get("xp").push({user: msgauthor, xp: 1}).write();
+    }else{
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb);
+        var userxp = Object.values(userxpdb)
+        console.log(userxp)
+        console.log(`nombre d'xp; ${userxp[1]}`)
+        
+        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
+
+        if(message.content === "sigbot!xp"){
+            var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+            var xpfinal = Object.values(xp);
+            var xp_embed = new Discord.RichEmbed()
+            .setTitle(`XP de ${message.author.username}`)
+            .setColor('0x0086AE')
+            .setDescription("Affichage des XP")
+            .addField("XP:", `${xpfinal[1]} xp`)
+            .setFooter(":D")
+            message.channel.send({embed: xp_embed});
+        }}
 if (message.content === "sigbot!gif"){
     message.channel.send("on ce met au image et au gif maintenant \n alors preparais vous car on a atein une nouvelle aire de jeux", {
         file: "https://media.giphy.com/media/LOtqITm3tFmiA/giphy.gif" 
