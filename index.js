2345149de82856e670e6c424b1e013309b1eb474
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2018-06-23 21:31:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -249,22 +249,7 @@ if(message.content === "Qui est Mokigrokaka ?"){
 })
 }
 if (message.content === "<@437179201284603904>" ){ 
-
-    const embed = new Discord.RichEmbed()
-    .setTitle("info sur Sigbot")
-    .setAuthor("Sig", "https://puyonexus.com/forum/download/file.php?avatar=1481_1311799010.png")
-    .setColor(0x0086AE)
-    .setDescription("le bot a éter crée juste pour faire de la programation , mais est partie bien plus loin que sa, il a été crée par @Sig qui c'est fais aidez par  @̶̶ۣۣۜۜ͜ζ͜͡▪) ρнคгยxтαภ  pour les petite erreur :D")
-    .setFooter("pour plus d'info demander au créateur du bot :3", "https://orig00.deviantart.net/24f3/f/2015/305/4/e/sig_as_amitie__by_grimtales1lizzybird-d9f6ez9.png")
-    .setImage("https://78.media.tumblr.com/9a209d8b2d826c25b70b0fa8e29aeb0f/tumblr_n96mqjeBdg1qmbwk5o1_r2_500.png")
-    .setThumbnail("https://puyonexus.com/mediawiki/images/d/d9/Img201007.png")
-    .setTimestamp()
-    .setURL("https://discordapp.com/api/oauth2/authorize?client_id=437179201284603904&permissions=2146958583&scope=bot")
-    .addField("le bot peu reagir avec 3 maniere ",
-      "un texte \n le mentionner \n et utiliser le prefix\nutiliser la commande sigbot!help pour plus d'info")
-    .addField("prefix du bot", "le prefix du bot est sigbot! mais il est possible qu'il change", false);
-  
-    message.channel.send({embed});
+//metre DM pour moi
 }
 if (message.content === prefix + "help" ){  
 
