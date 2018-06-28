a670921c6ce07c369cef9a7d22acfadf13598692
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2018-06-28 20:26:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -343,7 +343,7 @@ if(message.content === "sakamoto!help"){ //sakamoto help
     message.channel.send({embed});
 }
 if (message.content.startsWith("sigbot!dit")){
-    if("323807479651631104".includes(message.author.id)){
+    if("234368202379886593".includes(message.author.id)){
     let args = message.content.split(" ").slice(1)
     let thingToEcho = args.join(" ")
     message.delete();
