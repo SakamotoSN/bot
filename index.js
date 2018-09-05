749fc369b8130a1e80e1d49ef16ce64ec2122e12
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2018-09-05 20:27:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.on('message', message => {
         userToSend.send(`${message.author.username} vous a envoyer un message!\n${messageToSend}`);
         message.delete(`${message.author.username} vous a envoyer un message!\n${messageToSend}`);
         message.channel.send(`**${message.author}** , votre message a bien été envoyer a **${message.mentions.users.first().username}** :D`)
-        console.log(`un utilisateur a utiliser les DM :D ${message.author.username} a envoyer un message a ${message.mentions.users.first().username}  ${messageToSend}`)
+        console.log(`${message.author.username} a envoyer un message : ${messageToSend}`)
 }else{
     message.delete();
     message.channel.send(`erreur`)
